51ef565428468e6abff1579a005aa70706437a88
Taras <taras.lepa@gmail.com>
2021-07-21 21:24:42 +0400

    first task

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
-std::string name;
-int age;
+
 
 void print()
 {
-std::cout << "Hello. Who are you?" << std::endl;
+    std::cout << "Hello. Who are you?" << std::endl;
 }
 
 void answer()
 {
+    std::string name;
+    int age;
     print();
     std::cin >> name;
-    std::cout << "How are you years old?" << std::endl;
-    std::cin>>age;
-    std::cout<<name +" ,"<<age<< " is the best age!!!!!!!!!!!!!!"<<std::endl;
+    std::cout << "How are you old?" << std::endl;
+    std::cin >>  age;
+    std::cout << name << " ," <<age<< " is the best age!!!!!!!!!!!!!!"  <<std::endl;
 }
 
 int main()
 {
-answer();
+    answer();
 }
