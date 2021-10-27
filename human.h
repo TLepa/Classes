39683c0b73e90b1b709de83ec4d58e69bc029aa3
taras.lepa@gmail.com <taras.lepa@gmail.com>
2021-10-27 18:36:40 +0300

    My new attempt with classes.Firrs expirience work with files.

--- human.h
+++ human.h
@@ -2,7 +2,7 @@
 #define HUMAN_H
 #include<string>
 #include <iostream>
-
+#include <fstream>
 
 
 class Human
--- main.cpp
+++ main.cpp
@@ -2,37 +2,82 @@
 #include <vector>
 #include "teacher.h"
 
+//enum ParseResult
+//{
+//    SUCCESS = 0,
+//    ERROR_OPENING_FILE = -1,
+//    ERROR_READING_FILE = -3
+//};
 
+//ParseResult readFileContents()
+//{
+//    if (!openFile())
+//        return ERROR_OPENING_FILE;
+//    if (!readfile())
+//        return ERROR_READING_FILE;
 
-int main()
+//    return SUCCESS; // если всё прошло успешно
+//}
+
+int main(int argc, char *argv[])
 {
-    Teacher taras;
-    taras.setName("Taras");
-    taras.setZp (19000);
-    taras.setSubject ("Deutch");
 
-    Teacher natalia;
-    natalia.setName("Natalia");
-    natalia.setSubject ("English");
-    natalia.setZp (19500);
 
-    Teacher orest;
-    orest.setName("Orest");
-    orest.setZp (22000);
-    orest.setSubject ("French");
+    Teacher vasia;
+    vasia.setName(argv[1]);
+    vasia.setSubject(argv[2]);
+    vasia.setAge(atoi(argv[3]));
+    vasia.setZp(atoi(argv[4]));
+    //    Teacher taras;
+    //    taras.setName("Taras");
+    //    taras.setZp (19000);
+    //    taras.setSubject ("Deutch");
+
+    //    Teacher natalia;
+    //    natalia.setName("Natalia");
+    //    natalia.setSubject ("English");
+    //    natalia.setZp (19500);
+
+    //    Teacher orest;
+    //    orest.setName("Orest");
+    //    orest.setZp (22000);
+    //    orest.setSubject ("French");
 
     std::vector <Teacher> students;
-    students.push_back(taras);
-    students.push_back(natalia);
-    students.push_back(orest);
-
-    for (Teacher &element: students)
-    {
-        std::cout <<  element.getName()  << std::endl;
-        std::cout <<  element.getSubject()  << std::endl;
-        std::cout <<  element.getAge() << std::endl;
-        std::cout <<  element.getZp() << "\n" << std::endl;
+    //    students.push_back(taras);
+    //    students.push_back(natalia);
+    //    students.push_back(orest);
+    students.push_back(vasia);
+    //    for (Teacher &element: students)
+    //    {
+    //        std::cout <<  element.getName()  << std::endl;
+    //        std::cout <<  element.getSubject()  << std::endl;
+    //        std::cout <<  element.getAge() << std::endl;
+    //        std::cout <<  element.getZp() << "\n" << std::endl;
+    //    }
+    std::ofstream out;          // поток для записи
+    out.open("C:\\Users\\Natalya\\Desktop\\c++\\Classes\\newthread.txt"); // окрываем файл для записи
+    if (out.is_open())
+    {    for (Teacher &element: students)
+        {
+            out <<  element.getName()  << std::endl;
+            out <<  element.getSubject()  << std::endl;
+            out <<  element.getAge() << std::endl;
+            out <<  element.getZp() << "\n" << std::endl;
+        }
+        out.close();
     }
+    std::string line;
+
+    std::ifstream in("C:\\Users\\Natalya\\Desktop\\c++\\Classes\\newthread.txt"); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            std::cout << line << std::endl;
+        }
+    }
+    in.close();
 }
 
 
